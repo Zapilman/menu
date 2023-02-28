b12427edf8fe747d894171dd7ff3567a7607c365
Zapilman <17uraz12@gmail.com>
2023-02-28 20:36:17 +0200

    add mongo connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { getMongoConfig } from './configs/mongo.config';
 import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [AuthModule, RestaurantModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+    AuthModule,
+    RestaurantModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
