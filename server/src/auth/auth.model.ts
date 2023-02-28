2aee9911f77281e8287eac311d2f0a266a99b377
Zapilman <17uraz12@gmail.com>
2023-02-28 20:45:29 +0200

    change auth model

--- server/src/auth/auth.model.ts
+++ server/src/auth/auth.model.ts
@@ -1,4 +1,11 @@
-export class Auth {
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IAuthModel extends Base {}
+export class AuthModel extends TimeStamps {
+  @prop({ unique: true })
   login: string;
+
   passwordHash: string;
 }
