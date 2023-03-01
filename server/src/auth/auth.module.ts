89942a2b65e6a441a96faa885afc2f0e3cb4a383
Zapilman <17uraz12@gmail.com>
2023-03-01 20:23:51 +0200

    change auth service

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { getJwtConfig } from 'src/configs/jwt.config';
 import { AuthController } from './auth.controller';
 import { AuthModel } from './auth.model';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
@@ -9,6 +13,12 @@ import { AuthModel } from './auth.model';
     TypegooseModule.forFeature([
       { typegooseClass: AuthModel, schemaOptions: { collection: 'Auth' } },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigModule],
+      useFactory: getJwtConfig,
+    }),
   ],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -1,4 +1,4 @@
 export class AuthDto {
   login: string;
-  passwordHash: string;
+  password: string;
 }
