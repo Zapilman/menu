b96e87b47ffea96b8d75e91849c8b0419c418717
Zapilman <17uraz12@gmail.com>
2023-05-13 19:01:19 +0300

    change route

--- src/api/axiosInstanse.ts
+++ src/api/axiosInstanse.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstanse = axios.create({
-  baseURL: '127.0.0.1:8080/api',
+  baseURL: 'http://127.0.0.1:8080/api',
   timeout: 20000,
   headers: {
     'Content-type': 'application/json',
