3af609ff9fb63495616f323792975aaa1fe05649
Zapilman <17uraz12@gmail.com>
2023-05-28 07:03:27 +0300

    refactor translations

--- i18n/extract_translations.js
+++ i18n/extract_translations.js
@@ -1,4 +1,4 @@
-const { LokaliseApi } = require('@lokalise/node-api');
+// const { LokaliseApi } = require('@lokalise/node-api');
 
 const fs = require('fs');
 const path = require('path');
@@ -29,23 +29,23 @@ files.forEach((file) => {
   }
 });
 
-const jsonContent = JSON.stringify(translations, null, 2);
-fs.writeFileSync('translations.json', jsonContent, 'utf8');
+// const jsonContent = JSON.stringify(translations, null, 2);
+// fs.writeFileSync('translations.json', jsonContent, 'utf8');
 
-(async () => {
-  const lokaliseApi = new LokaliseApi({
-    apiKey: 'b319d056c11b5779919044529c75d9c3db60a45d',
-  });
-  const projects = await lokaliseApi.projects().list();
-  projects.items[0].name;
-  console.log(projects.items[0].name);
+// (async () => {
+//   const lokaliseApi = new LokaliseApi({
+//     apiKey: 'b319d056c11b5779919044529c75d9c3db60a45d',
+//   });
+//   const projects = await lokaliseApi.projects().list();
+//   projects.items[0].name;
+//   console.log(projects.items[0].name);
 
-  process = await lokaliseApi.files().upload(project_id, {
-    data: data_base64,
-    filename: 'translations.json',
-    lang_iso: 'en',
-  });
-  process.status; // => 'queued'
+//   process = await lokaliseApi.files().upload(project_id, {
+//     data: data_base64,
+//     filename: 'translations.json',
+//     lang_iso: 'en',
+//   });
+//   process.status; // => 'queued'
 
-  console.log(translations);
-})();
+//   console.log(translations);
+// })();
